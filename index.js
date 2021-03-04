013922d0031859bc810445c369cf5b3152062085
Akhilesh Goswami <agiri5375@gmail.com>
2021-03-04 12:16:12 +0600

    Portfolio

---
+++ index.js
@@ -0,0 +1,10 @@
+alert('Please give your feedback😀!')
+function post() {
+    if (window.confirm("Are you sure you want to submit?")) {
+        alert("Thanks for Your Feedback😊")
+        window.open("#home");
+    }
+    else {
+        window.open("#home");
+    }
+}
